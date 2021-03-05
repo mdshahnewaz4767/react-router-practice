ce0e01b7c21794c55fe8d6688e31ac2baa5eb5a1
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-06 02:05:29 +0600

    create route parameter and read from route parameter

--- src/components/FriendDetails/FriendDetails.js
+++ src/components/FriendDetails/FriendDetails.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const FriendDetails = () => {
+    const {friendId} = useParams();
     return (
         <div>
-            <h2>This is friend details components</h2>
+            <h2>This is Friend Details Component: {friendId}</h2>
         </div>
     );
 };
